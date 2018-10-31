9daaf82ff8f92ae9b9ec313661946f3f067887b9
Omar <oa.azab@gmail.com>
2018-11-01 01:24:25 +0200

    Migrate to AndroidX

--- app/src/androidTest/java/omar/example/com/wallofimages/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/omar/example/com/wallofimages/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package omar.example.com.wallofimages
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/main/java/omar/example/com/wallofimages/MainActivity.kt
+++ app/src/main/java/omar/example/com/wallofimages/MainActivity.kt
@@ -1,6 +1,6 @@
 package omar.example.com.wallofimages
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
